a5113c670e46b7f0c9b1246f077e953b5d2170b9
Robert <robertxluo@gmail.com>
2018-09-14 17:35:46 -0700

    Add Redux action generators

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/app.js',
+    entry: './src/playground/redux-expensify.js',
     output: {
         path: path.resolve(__dirname, 'public/scripts'),
         filename: 'bundle.js'
