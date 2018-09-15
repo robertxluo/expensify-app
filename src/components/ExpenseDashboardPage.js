cbc0c16388f6005da50f0eb67004a54e1bf2a51c
Robert <robertxluo@gmail.com>
2018-09-15 12:33:34 -0700

    Add filter input box to filter expenses properly

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ExpenseList from './ExpenseList';
-import ExpenseListItem from './ExpenseListItem';
+import ExpenseListFilters from './ExpenseListFilters';
 
 const ExpenseDashboardPage = () => (
     <div>
+        <ExpenseListFilters />
         <ExpenseList />
-        <ExpenseListItem />
     </div>
 );
 
