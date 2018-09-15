a768ee2d54f90581f85ddfc783afbea9694b8ece
Robert <robertxluo@gmail.com>
2018-09-15 16:33:51 -0700

    Set up calendar date picker and added customization for it

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,19 +1,26 @@
 import React from 'react';
+import moment from 'moment';
+import 'react-dates/initialize'
+import { SingleDatePicker } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
+
 
 export default class ExpenseForm extends React.Component {
     state = {
         description: '',
         note: '',
-        amount: ''
+        amount: '',
+        createdAt: moment(),
+        calendarFocused: false
     };
     onDescriptionChange = (e) => {
         const description = e.target.value;
         this.setState(() => ({ description }));
-    }
+    };
     onNoteChange = (e) => {
         const note = e.target.value;
         this.setState(() => ({ note }));
-    }
+    };
     onAmountChange = (e) => {
         const amount = e.target.value;
         if (amount.match(/^\d*(\.\d{0,2})?$/))
@@ -21,6 +28,12 @@ export default class ExpenseForm extends React.Component {
             this.setState(() => ({ amount }));
         }
     };
+    onDateChange = (createdAt) => {
+        this.setState(() => ({ createdAt}))
+    };
+    onFocusChange = ({ focused }) => {
+        this.setState(() => ({ calendarFocused: focused}))
+    }
     render() {
         return (
             <div>
@@ -37,6 +50,14 @@ export default class ExpenseForm extends React.Component {
                         placeholder="Amount"
                         value={this.state.amount}
                         onChange={this.onAmountChange}
+                    />
+                    <SingleDatePicker 
+                        date={this.state.createdAt}
+                        onDateChange={this.onDateChange}
+                        focused={this.state.calendarFocused}
+                        onFocusChange={this.onFocusChange}
+                        numberOfMonths={1}
+                        isOutsideRange={(day) => false}
                     />
                     <textarea
                         placeholder="Add a note for your expense (optional)"
