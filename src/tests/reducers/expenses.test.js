bc58aeb7d9732f40bab76e62fca85a3a6cd797a8
Robert <robertxluo@gmail.com>
2018-09-30 14:56:14 -0700

    Move dummy expenses data to separate fixtures file

---
+++ src/tests/reducers/expenses.test.js
@@ -0,0 +1,7 @@
+import expensesReducer from '../../reducers/expenses.js';
+import expenses from '../fixtures/expenses';
+
+test('should set default state', () => {
+    const state = expensesReducer(undefined, { type: '@@INIT' });
+    expect(state).toEqual([]);
+});
