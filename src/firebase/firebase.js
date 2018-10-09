70609ec0c9ba0a29a2c968a665c4f149c1eee77b
Robert <robertxluo@gmail.com>
2018-10-09 13:35:29 -0700

    Create separate test database

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: "AIzaSyD-pFIP4cgLcvsH3PYekvIfKRQ6FInPNjw",
-  authDomain: "expensify-a6277.firebaseapp.com",
-  databaseURL: "https://expensify-a6277.firebaseio.com",
-  projectId: "expensify-a6277",
-  storageBucket: "expensify-a6277.appspot.com",
-  messagingSenderId: "1092931883646"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
